0d910f419fcd1a200c515c179f9265229751cf97
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-26 19:34:52 +0530

    [shreya]Added:Invoice Summary class to generate invoice

--- main/java/com/capgemini/CabInvoiceService.java
+++ main/java/com/capgemini/CabInvoiceService.java
@@ -11,12 +11,12 @@ public class CabInvoiceService {
 	 return Math.max(totalFare, MIN_FARE);
 	}
     
-    public double calculateFare(Ride[] rides) {
+    public InvoiceSummary calculateFare(Ride[] rides) {
 		double totalFare = 0.0;
 		for(Ride ride : rides) {
 			totalFare += this.calculateFare(ride.distnce, ride.time);
 		}
-		return totalFare;
+		return new InvoiceSummary(rides.length, totalFare);
 	}
 	
 	
